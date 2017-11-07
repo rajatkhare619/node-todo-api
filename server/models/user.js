31a5f66ff257c6e2772319373974617741eeabcd
Rajat Khare <rajatkhare619@gmail.com>
2017-11-07 22:24:58 +0530

    added post /todos route and refactored mongoose

---
+++ server/models/user.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+
+let User = mongoose.model('User', {
+    email: {
+        required: true,
+        trim: true,
+        type: String,
+        minlength: 1
+    }
+});
+
+module.exports = {User};
