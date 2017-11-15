2f5d8b2638b3ef8ae0a6f97bd32bd87352b2667e
Rajat Khare <rajatkhare619@gmail.com>
2017-11-15 21:04:52 +0530

    changed mongo server for heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp'); //Local
-//mongoose.connect('mongodb://heroku:123@ds259325.mlab.com:59325/herokuapp'); //Online
+//mongoose.connect('mongodb://localhost:27017/TodoApp'); //Local
+mongoose.connect('mongodb://heroku:123@ds259325.mlab.com:59325/herokuapp'); //Online
 
 module.exports = {
     mongoose
