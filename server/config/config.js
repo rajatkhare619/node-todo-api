0e8c715b234515744dcfc183980ab01aa7c02d7d
Rajat Khare <rajatkhare619@gmail.com>
2017-11-24 20:03:59 +0530

    updated config to use JSON file

--- server/config/config.js
+++ server/config/config.js
@@ -1,5 +1,14 @@
 let env = process.env.NODE_ENV || "development";
 
+if (env === "development" || env === "test") {
+    let config = require('./config.json');
+    let envConfig = config[env];
+    Object.keys(envConfig).forEach((key) => {
+        process.env[key] = envConfig[key];
+    });
+}
+
+/*
 if (env === "development") {
     process.env.PORT = 3000;
     process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
@@ -7,3 +16,4 @@ if (env === "development") {
     process.env.PORT = 3000;
     process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
 }
+*/
