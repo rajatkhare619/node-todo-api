9f1eeac6344a187ba36f3c6d6f396434de54db96
Rajat Khare <rajatkhare619@gmail.com>
2017-11-25 18:46:53 +0530

    changed connection for heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 //mongoose.connect(process.env.MONGODB_URI); //Local
-mongoose.connect('mongodb://heroku:123@ds259325.mlab.com:59325/herokuapp'); //Online
+mongoose.connect('mongodb://heroku:123456@ds259325.mlab.com:59325/herokuapp'); //Online
 
 module.exports = {
     mongoose
